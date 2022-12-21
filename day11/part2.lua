fa0a5e6794307ffa9cc0c20800199d311d70bcd2
Maxim Minaev <minmax1996@mail.ru>
2022-12-22 01:55:35 +0400

    day11

---
+++ day11/part2.lua
@@ -0,0 +1,67 @@
+local function CreateMonkey(s_items, operation, test_div, t_receiver, f_receiver)
+	local items = {}
+	for i in s_items:gmatch("(%d+)") do
+		items[#items + 1] = tonumber(i)
+	end
+	return {
+		items = items,
+		add_item = function(self, item)
+			self.items[#self.items + 1] = item
+		end,
+		get_worry_level = function(item)
+			local funcStr = "return function(old) return " .. operation .. " end"
+			local execute = select(2, pcall(select(1, load(funcStr))))
+			return execute(item)
+		end,
+		test = function(worry_level)
+			if math.fmod(worry_level, test_div) == 0 then
+				return tonumber(t_receiver)
+			else
+				return tonumber(f_receiver)
+			end
+		end,
+	}
+end
+
+local monkeys = {}
+
+local X = 1
+local f = io.open("input.txt")
+local monkestr = ""
+while f ~= nil do
+	local line = f:read("L")
+	if line == "\n" or line == nil then
+		local div = tonumber(monkestr:match("Test: divisible by (%d+)"))
+		monkeys[#monkeys + 1] = CreateMonkey(
+			monkestr:match("Starting items: ([^\n]+)"),
+			monkestr:match("Operation: new = ([^\n]+)"),
+			div,
+			monkestr:match("If true: throw to monkey (%d+)"),
+			monkestr:match("If false: throw to monkey (%d+)")
+		)
+		X = X * div
+		monkestr = ""
+	else
+		monkestr = monkestr .. line
+	end
+	if line == nil then
+		break
+	end
+end
+
+local tests = {}
+for round = 1, 10000, 1 do
+	for m_i = 1, #monkeys, 1 do
+		for k, item in pairs(monkeys[m_i].items) do
+			local worry_level = monkeys[m_i].get_worry_level(item)
+			worry_level = math.fmod(worry_level, X)
+			local monkey_r = monkeys[m_i].test(worry_level)
+			monkeys[monkey_r + 1]:add_item(worry_level)
+		end
+		tests[m_i] = (tests[m_i] or 0) + #monkeys[m_i].items
+		monkeys[m_i].items = {}
+	end
+end
+
+table.sort(tests)
+print("MonkeyBusiness: ", tests[#tests] * tests[#tests - 1])
